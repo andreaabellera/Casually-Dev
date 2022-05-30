988d0826af62131b67d9d85eb3ddf4a6b5011fcb
Andrea Abellera <andrea.abellera@gmail.com>
2022-05-30 05:42:06 -0500

    Pure sorcery

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import App from "./App.svelte";
+import App from "./App.js";
 
 var app = new App({
 	target: document.body
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,5 @@ export default defineConfig({
     server: {
         port: 5000,
     },
-    
     plugins: [svelte(), yaml()]
 });
