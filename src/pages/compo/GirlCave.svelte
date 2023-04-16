7a75cef7e2b3f6cfacb6375d4c5f3794693bffbd
Andrea Abellera <andrea.abellera@gmail.com>
2023-04-16 17:26:34 -0500

    Add videos to the cave
    Will fix dimension scaling later

--- src/pages/compo/GirlCave.svelte
+++ src/pages/compo/GirlCave.svelte
@@ -13,8 +13,23 @@
         <ProjectFeat />
     </div>
     <div class="generic-ctr">
-        <iframe id="casually-css-roadshow0" style="width:100%;" height="500" src="https://www.youtube-nocookie.com/embed/zKJXCX3J_-k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe height="450" width="800" src="https://www.youtube-nocookie.com/embed/zKJXCX3J_-k" title="Casually CSS Teaser" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
     </div>
+    <div class="generic-ctr">
+        <iframe height="450" width="400" src="https://fleek.ipfs.io/ipfs/bafybeiasfyuculte5cldzl4g3numniwjlfiua466vx25ot34cvcvbehdsq" title="The Conception of Baldy" frameborder="0" allowfullscreen />
+    </div>
+    <div class="generic-ctr">
+        <iframe height="450" width="800" src="https://www.youtube-nocookie.com/embed/JKZDh6bA_3Y" title="Recollection Day" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen />
+    </div>
+    <div class="generic-ctr">
+        <iframe width="450" height="800" src="https://www.youtube-nocookie.com/embed/dXBGJ2Iq1uQ" title="Farmer's Life" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen />
+    </div>
+    <div class="generic-ctr">
+        <iframe height="450" width="800" src="https://ipfs.fleek.co/ipfs/bafybeiaflcmffan2magm7xvccpn2swfecf442zqpx6rhmagyessrtvgwse" title="Farmony" frameborder="0" allowfullscreen />
+    </div>
+    <!-- <div class="generic-ctr">
+        <iframe height="450" width="800" src="https://ipfs.fleek.co/ipfs/bafybeidvyc6vfqtds5wsuixeisisuc4s674pecok6s2ifwmfigyd6xweoi" frameborder="0" allowfullscreen title="Delphi Tech Corp Afterparty" />
+    </div> -->
 </div>
 
 <style>
@@ -49,9 +64,10 @@
         justify-items: center;
     }
 
-    #casually-css-roadshow0{
-        width: 90%;
-        max-width: 900px;
+    .generic-ctr iframe{
+        width: 800px;
+        height: 400px;
+        max-width: 100%;
         margin: 4vh 0 4vh 0;
     }
 
