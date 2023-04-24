b6a5e5bf7f2cee74ff9a5989e55f1a6426dcc9bb
Andrea Abellera <andrea.abellera@gmail.com>
2023-04-23 23:29:16 -0500

    Revert "Test emojiless for Iphone bug"
    This reverts commit 5e658f39aedd2968503d4f13b4020052e365d419.

--- src/pages/blog/index.svelte
+++ src/pages/blog/index.svelte
@@ -6,9 +6,9 @@
     import blogData from '../content/blogs.yml'
 
     // Show and hide blogs based on filters
-    let categories = ["Develop", "Programming", "Art", "Life", "Games", "Random"]
+    let categories = ["Develop ⚙️", "Programming 👩🏻‍💻", "Art 🖼️", "Life 💃🏻", "Games 🎲", "Random 🍡"]
     let filters = categories
-    function fselect(){
+    function fselect(event){
         let classes = this.classList
         if(classes.contains("fselect")){
             this.classList.remove("fselect")
