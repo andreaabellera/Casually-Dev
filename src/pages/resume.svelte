0e7c1887c56e199b0c4b27ea07160fd8ba0dc4e0
Andrea Abellera <andrea.abellera@gmail.com>
2023-05-05 16:05:45 -0500

    Add resume route

---
+++ src/pages/resume.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(()=>{ document.getElementById("resume-direct").click() })
+</script>
+
+<a id="resume-direct" href="https://ipfs.io/ipfs/bafybeienupcvre4ggipbkefeym7f42nta4kzud6g5r7vrl57ifyevrzytm">redirecting..</a>
+
+
+<div class="intermission">
+    <a id="resume-direct" class="pretty-text laBelleAurore" href="https://ipfs.io/ipfs/bafybeienupcvre4ggipbkefeym7f42nta4kzud6g5r7vrl57ifyevrzytm">redirecting..</a>
+</div>
+
+<style>
+    .intermission {
+        height: max-content;
+        width: 100%;
+        display: grid;
+        grid-template-rows: auto auto;
+        margin: 10vh 0 15vh 0;
+        justify-items: center;
+        text-align: center;
+    }
+
+    .pretty-text {
+        font-size: 2em;
+        margin-top: 1.5vh;
+        width: 90%;
+        color: var(--coffee);
+    }
+</style>
