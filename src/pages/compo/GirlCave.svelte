1095451b77d29cbe5af9ba29a78dfb7f3c0ccbc1
Andrea Abellera <andrea.abellera@gmail.com>
2023-05-15 18:56:04 -0500

    Add first WebVTT video captioning

--- src/pages/compo/GirlCave.svelte
+++ src/pages/compo/GirlCave.svelte
@@ -111,7 +111,7 @@
             
             <video controls style="width:100%;" height="500" aria-label="Life as a Delphi Tech Dev" poster="https://ipfs.io/ipfs/bafybeicqp5pku4c2qifv7sc5jseapvq2orr5d2ykne7subapic47ne3b7u">
                 <source src="https://ipfs.fleek.co/ipfs/bafybeidvyc6vfqtds5wsuixeisisuc4s674pecok6s2ifwmfigyd6xweoi" type="video/mp4">
-                <track kind="captions"/>
+                <track src="/captions/delphiInternAfterparty.vtt" label="English" kind="captions" srclang="en-us" default>
                 <p>
                     Your browser doesn't support HTML video. Here is a <a href="https://ipfs.fleek.co/ipfs/bafybeidvyc6vfqtds5wsuixeisisuc4s674pecok6s2ifwmfigyd6xweoi">link to the video</a> instead.
                 </p>
